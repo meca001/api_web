1fc6a7de881954f09ed6eb18cfcf263ed3842e2c
meca001 <72147194+meca001@users.noreply.github.com>
2023-12-18 21:31:07 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servidor = 'localhost';
+$usuario = 'root';
+$contra = '';
+$db_nombre = 'webapi';
+
+$conn = mysqli_connect($servidor, $usuario, $contra, $db_nombre);
+
+?>
+
+
+
+
---
+++ index.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+    $pais = isset($_POST['pais']);
+    echo $pais
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div></div>
+    <h1>Ingresa el pais que quieres saber el clima en ingles</h1>
+    <form action="webApi.php" method="POST">
+            <input type="text" name="pais" id="">
+            <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
